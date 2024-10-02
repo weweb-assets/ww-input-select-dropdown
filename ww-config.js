6654e29797b4f1f529b5d6afcce0826fbb63d7b2
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-10-02 18:35:06 +0200

    Init

--- ww-config.js
+++ ww-config.js
@@ -1,11 +1,14 @@
 export default {
     editor: {
-        label: 'Select',
+        label: 'Select Dropdown',
         icon: 'select',
-        bubble: true,
     },
     inherit: {
         type: 'ww-layout',
+    },
+    options: {
+        autoByContent: true,
+        displayAllowedValues: ['flex', 'grid', 'inline-flex', 'inline-grid'],
     },
     states: ['readonly'],
     triggerEvents: [
@@ -13,174 +16,97 @@ export default {
         { name: 'initValueChange', label: { en: 'On init value change' }, event: { value: '' } },
     ],
     properties: {
-        items: {
-            bindable: 'repeatable',
-            label: {
-                en: 'Items',
-                fr: 'Items',
-            },
-            type: 'Info',
-            options: {
-                text: {
-                    en: 'Elements to repeat',
-                },
-            },
-            defaultValue: [],
-            /* wwEditor:start */
-            bindingValidation: {
-                validations: [
-                    {
-                        type: 'array',
-                    },
-                    {
-                        type: 'object',
-                    },
-                ],
-                tooltip:
-                    'A collection or an array of data: \n\n`myCollection` or `[{}, {}, ...] || ["string1", "string2", ...] || [1, 2, ...]`',
-            },
-            /* wwEditor:end */
-        },
-        value: {
-            type: 'Text',
-            label: 'Initial value',
-            settings: true,
-            bindable: true,
-            /* wwEditor:start */
-            bindingValidation: {
-                validations: [
-                    {
-                        type: 'array',
-                    },
-                    {
-                        type: 'string',
-                    },
-                ],
-                tooltip: 'A string value: \n\n`"myValue"`, or an array of values: \n\n`["myValue1", "myValue2"]`',
-            },
-            /* wwEditor:end */
-        },
-        name: {
-            type: 'Text',
-            label: 'Radio name',
-            settings: true,
-            bindable: true,
-            /* wwEditor:start */
-            bindingValidation: {
-                type: 'string',
-                tooltip: 'A string value for the radio name: \n\n`"myRadioName"`',
-            },
-            /* wwEditor:end */
-        },
-        selectType: {
-            label: 'Select type',
+        side: {
+            label: { en: 'Side' },
             type: 'TextSelect',
             options: {
                 options: [
-                    {
-                        value: 'single',
-                        label: 'Single',
-                    },
-                    {
-                        value: 'multiple',
-                        label: 'Multiple',
-                    },
+                    { value: 'bottom', label: 'Bottom' },
+                    { value: 'left', label: 'Left' },
+                    { value: 'top', label: 'Top' },
+                    { value: 'right', label: 'Right' },
                 ],
             },
-            bindable: true,
-            /* wwEditor:start */
-            bindingValidation: {
-                type: 'string',
-                enum: ['single', 'multiple'],
-                tooltip: 'A string value, either "single" or "multiple"',
-            },
-            /* wwEditor:end */
-        },
-        disabled: {
-            label: { en: 'Disabled' },
-            type: 'OnOff',
+            defaultValue: 'bottom',
             section: 'settings',
-            defaultValue: false,
-            bindable: true,
-            /* wwEditor:start */
-            bindingValidation: {
-                type: 'boolean',
-                tooltip: 'A boolean value: \n\n`true` or `false`',
-            },
-            /* wwEditor:end */
         },
-        required: {
-            label: { en: 'Required' },
-            type: 'OnOff',
-            section: 'settings',
-            defaultValue: false,
-            bindable: true,
-            /* wwEditor:start */
-            bindingValidation: {
-                type: 'boolean',
-                tooltip: 'A boolean value: \n\n`true` or `false`',
+        align: {
+            label: { en: 'Align' },
+            type: 'TextSelect',
+            options: {
+                options: [
+                    { value: 'start', label: 'Start' },
+                    { value: 'center', label: 'Center' },
+                    { value: 'end', label: 'End' },
+                ],
             },
-            /* wwEditor:end */
-        },
-        readonly: {
-            label: { en: 'Read-only' },
-            type: 'OnOff',
+            defaultValue: 'start',
             section: 'settings',
-            defaultValue: false,
-            bindable: true,
-            /* wwEditor:start */
-            bindingValidation: {
-                type: 'boolean',
-                tooltip: 'A boolean value: \n\n`true` or `false`',
+        },
+        offsetX: {
+            label: { en: 'Offset X' },
+            type: 'Length',
+            options: {
+                unitChoices: [
+                    { value: 'px', label: 'px', min: -100, max: 100 },
+                    { value: '%', label: '%', min: -100, max: 100 },
+                ],
             },
-            /* wwEditor:end */
-        },
-        canUnselect: {
-            label: { en: 'Can unselect' },
-            type: 'OnOff',
+            defaultValue: '0px',
             section: 'settings',
-            defaultValue: false,
-            bindable: true,
-            /* wwEditor:start */
-            bindingValidation: {
-                type: 'boolean',
-                tooltip: 'A boolean value: \n\n`true` or `false`',
+        },
+        offsetY: {
+            label: { en: 'Offset Y' },
+            type: 'Length',
+            options: {
+                unitChoices: [
+                    { value: 'px', label: 'px', min: -100, max: 100 },
+                    { value: '%', label: '%', min: -100, max: 100 },
+                ],
             },
-            /* wwEditor:end */
-        },
-        forceOpenInEditor: {
-            label: { en: 'Force open in editor' },
-            type: 'OnOff',
+            defaultValue: '0px',
             section: 'settings',
+        },
+        initialState: {
+            label: { en: 'Initial state' },
+            type: 'OnOff',
             defaultValue: false,
+            section: 'settings',
+        },
+        editorDisplay: {
+            label: { en: 'Editor display' },
+            type: 'OnOff',
+            defaultValue: false,
+            section: 'settings',
             editorOnly: true,
         },
-        loadOnScroll: {
-            label: { en: 'Load on scroll' },
+        autoPosition: {
+            label: { en: 'Auto position' },
             type: 'OnOff',
+            defaultValue: true,
             section: 'settings',
-            defaultValue: false,
-            bindable: true,
-            /* wwEditor:start */
-            bindingValidation: {
-                type: 'boolean',
-                tooltip: 'A boolean value: \n\n`true` or `false`',
-            },
-            /* wwEditor:end */
         },
-        options: {
-            label: { en: 'Options' },
-            type: 'Array',
-            section: 'settings',
-            bindable: true,
-            defaultValue: [],
-            /* wwEditor:start */
-            bindingValidation: {
-                type: 'array',
-                tooltip:
-                    'An array of option objects: \n\n`[{ label: "Option 1", value: "1" }, { label: "Option 2", value: "2" }]`',
+        boundOffset: {
+            label: { en: 'Bound offset' },
+            type: 'Length',
+            options: {
+                unitChoices: [
+                    { value: 'px', label: 'px', min: 0, max: 100 },
+                    { value: '%', label: '%', min: 0, max: 100 },
+                ],
             },
-            /* wwEditor:end */
+            defaultValue: '0px',
+            section: 'settings',
+        },
+        animated: {
+            label: { en: 'Animated' },
+            type: 'OnOff',
+            defaultValue: true,
+            section: 'settings',
+        },
+        slot: {
+            hidden: true,
+            defaultValue: [],
         },
     },
 };
